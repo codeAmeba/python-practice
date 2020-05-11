82749376d4e6f3ea3dd3e52922dd0c6fe85cd4e5
jeongsooyoung <code.ameba@gmail.com>
2020-05-12 00:38:00 +0900

    FEATURE: default value, return

--- function.py
+++ function.py
@@ -20,4 +20,16 @@ def minus(a, b=3):
 plus(3, 5)
 minus(5)
 
-# returns
+# return
+# return하면 함수는 값을 내놓고 종료되며 return 아래의 코드는 실행되지 않음 -> JS와 동일
+def p_plus(a, b):
+  print(a + b)
+
+def r_plus(a, b):
+  return a + b
+
+p_result = p_plus(3, 6)
+r_result = r_plus(3, 6)
+
+print(p_result) # 9 None -> return이 없으므로 print를 두 번 실행, 그래서 두 번째 print는 None
+print(r_result) # 9
