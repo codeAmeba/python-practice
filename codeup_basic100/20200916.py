987fc23ac5dcea7641a8ace2f23c58856f3d82e8
jeongsooyoung <code.ameba@gmail.com>
2020-09-16 22:52:50 +0900

    new directory

---
+++ codeup_basic100/20200916.py
@@ -0,0 +1,29 @@
+# 1. Hello 출력
+print("Hello")
+
+# 2. Hello World -> 공백이 포함된 문장 출력
+print("Hello World")
+
+# 3. Hello
+#    World -> 줄바꿈 출력
+print("Hello\nWorld")
+
+# 4. 'Hello' ->  작은 따옴표 포함하여 출력
+print("\'Hello\'")
+
+# 5. "Hello World" ->  큰 따옴표 포함하여 출력
+print("\"Hello World\"")
+
+# 6. "!@#$%^&*()" -> 특수문자 출력, 큰따옴표 포함
+print("\"!@#$%^&*()\"")
+
+# 7. "C:\Download\hello.cpp" -> 윈도우의 파일경로 출력, 큰따옴표 포함
+print("\"C:\\Download\\hello.cpp\"")
+
+# 8. 아래와 동일한 형태로 특수문자 출력
+# ┌┬┐
+# ├┼┤
+# └┴┘
+print("\u250c\u252c\u2510\n\u251c\u253c\u2524\n\u2514\u2534\u2518")
+
+# 9. 정수형 변수를 출력
