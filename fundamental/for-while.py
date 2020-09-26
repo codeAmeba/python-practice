3006220064a033f60fa717d89dff8151ca1bbe40
jeongsooyoung <code.ameba@gmail.com>
2020-09-26 18:11:16 +0900

    for and while

---
+++ fundamental/for-while.py
@@ -0,0 +1,10 @@
+for waiting_no in [0, 1, 2, 3, 4]:
+    print("대기번호 : {0}".format(waiting_no))
+
+for waiting_no2 in range(1, 6):
+    print("대기번호 : {0}".format(waiting_no2))
+
+starbucks_guest = ["ironman", "thor", "groot", "hulk"]
+
+for guest in starbucks_guest:
+    print("{0}'s drink is here".format(guest))
