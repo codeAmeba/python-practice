7ccedfff22149e5e8a9f2751ec4d2e7efdacf1f4
jeongsooyoung <code.ameba@gmail.com>
2020-09-27 22:46:11 +0900

    quiz using for and if

--- fundamental/for-while.py
+++ fundamental/for-while.py
@@ -1,4 +1,5 @@
 # for
+from random import *
 for waiting_no in [0, 1, 2, 3, 4]:
     print("대기번호 : {0}".format(waiting_no))
 
@@ -39,3 +40,22 @@ while person != customer:
 students = ["Ironman", "Hulk", "Thor", "Doctor Strange"]
 students = [i.upper() for i in students]
 print(students)
+
+
+# QUIZ: 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하시오
+# 조건1. 승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해짐
+# 조건2. 당신은 소요 시간 5분 ~ 15분 사이의 승객만 매칭해야 함
+# 출력 예시: [0] 1번째 손님 (소요시간 : 15분)
+# ...
+# 총 탐승 승객 : 2명
+
+cnt = 0  # 총 탐승 승객 수
+for i in range(1, 51):  # 1 ~ 50 승객
+    time = randrange(5, 51)  # 5 ~ 50 소요 시간
+    if 5 <= time <= 15:
+        print("[O] {0} 번째 손님 (소요 시간 : {1} 분)".format(i, time))
+        cnt += 1
+    else:
+        print("[ ] {0} 번째 손님 (소요 시간 : {1} 분)".format(i, time))
+
+print("총 탐승 승객 : {0} 명".format(cnt))
